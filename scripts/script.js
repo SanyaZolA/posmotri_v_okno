e2b61d0ca193735365b9bbfdf9a85e51e317c91c
Alexandr <ntl1221@yandex.ru>
2025-08-11 16:56:41 +0300

    fix UI and style

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 /* КОНФИГ */
 const preloaderWaitindTime = 1200;
-const cardsOnPage = 5;
+const cardsOnPage = 2;
 const BASE_URL = 'https://v-content.practicum-team.ru';
 const endpoint = `${BASE_URL}/api/videos?pagination[pageSize]=${cardsOnPage}&`;
 
